074b0438781572fe869b9b173f42db076d1a74b8
extgdi <georgios.dimitriadis@ica.se>
2017-04-11 13:04:31 +0200

    Added missing folders in build

--- src/js/GameViews/startview-component.js
+++ src/js/GameViews/startview-component.js
@@ -4,7 +4,7 @@ const StartPageComponent = () => {
   return (
     <div className="startpage-container">
       <h1>Hello world</h1>
-      <p>Please not that this application is in alpha stage and not yet finished</p>
+      <p>Please not that this application is in alpha stage and not yet finished!</p>
     </div>
   )
 }
