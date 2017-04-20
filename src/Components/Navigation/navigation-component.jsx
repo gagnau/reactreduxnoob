c778c812f4cde5149bbcd5723eb91eb74d8ffd5a
Georgios Dimitriadis CH <gedi01@gedimbp.ad.comhem.com>
2017-04-20 16:44:47 +0200

    fix(router): New file structure

--- src/Components/Navigation/navigation-component.jsx
+++ src/Components/Navigation/navigation-component.jsx
@@ -2,15 +2,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Route, Link } from 'react-router-dom'
-import StartPage from '../../Views/start-page'
-import Counter from '../../Views/counter'
+import StartPage from '../../Views/StartPage/start-page'
+import CounterPage from '../../Views/CounterPage/counter-page'
 
-const NavigationComponent = (({ currentItem }) =>
+const NavigationComponent = (() =>
   <nav className="navigation">
-  	<Link to={`/`} className="navigation__item navigation__item--selected">Start page {currentItem}</Link>
+  	<Link to={`/`} className="navigation__item navigation__item--selected">Start page</Link>
   	<Link to={`/page2`} className="navigation__item">Page 2</Link>
+
     <Route exact={true} path="/" component={StartPage} />
-    <Route path="/page2" component={Counter} />
+    <Route path="/page2" component={CounterPage} />
   </nav>
 )
 
--- src/Views/StartPage/start-page.jsx
+++ src/Views/StartPage/start-page.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import CounterContainer from '../Components/Counter/counter-container'
+import CounterContainer from '../../Components/Counter/counter-container'
 
 const StartPageComponent = (() =>
   <div className="start-page">
     <h2>Startpage</h2>
-    <h1>A simple React/Redux application</h1>
     <CounterContainer />
   </div>
 )
